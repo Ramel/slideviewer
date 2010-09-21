82ecfdf61536fbd7796e69524dca6257a5adf5be
Armel FORTUN <armel@maar.fr>
2010-09-21 19:54:25 +0200

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,31 @@
+# -*- coding: UTF-8 -*-
+# Copyright (C) 2007-2008 Juan David Ibáñez Palomar <jdavid@itaapy.com>
+# Copyright (C) 2008 Nicolas Deram <nicolas@itaapy.com>
+# Copyright (C) 2010 Armel FORTUN <armel@maar.fr>>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Import from itools
+from itools.core import get_abspath
+from itools.core import get_version
+
+# Import from ikaaro
+from ikaaro.skins import register_skin
+
+# The version
+__version__ = get_version()
+
+# Register skin
+path = get_abspath('ui')
+register_skin('slideviewer', path)
